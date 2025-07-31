6f5230a071d5505944916cbf374cc7b0d0354984
LUAN SILVA <luansilva8521@outlook.com.br>
2025-07-31 19:15:26 -0300

    Delete src/routes/deletepubliRoutes.js

--- src/routes/deletepubliRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const publicacaoController = require('../controllers/publicacaoController');
-const autenticarUsuario = require('../middleware/auth'); 
-
-
-
-router.delete('/publicacao/:id', autenticarUsuario, publicacaoController.deletarPublicacao); 
-
-module.exports = router;
