f14eb31999b729d5d0ab351e533ce9579b44a4ce
Luan Silva Dev 0fc <luansilva8521@outlook.com.br>
2025-08-02 18:21:43 -0300

    ATUALIZADO

--- src/routes/deletePostRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { deletarPublicacao } = require('../controllers/deletarPublicacaoController');
-const authMiddleware = require('../middleware/auth');  // Middleware de autenticação
-
-// Rota para deletar publicação
-router.delete('/:postId', authMiddleware, deletarPublicacao);
-
-module.exports = router;
